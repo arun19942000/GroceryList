e1635596bdb8c4d6d336164de0ada75d5ec43f0c
arun19942000 <89211054+arun19942000@users.noreply.github.com>
2021-11-18 10:41:13 +0530

    Add files via upload

---
+++ ajax.js
@@ -0,0 +1,53 @@
+function CreateTableFromJSON() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var grocerylist = JSON.parse(this.responseText);
+            var col = [];
+            for (var i = 0; i < grocerylist.length; i++) {
+                for (var key in grocerylist[i]) {
+                    if (col.indexOf(key) === -1) {
+                        col.push(key);
+                    }
+                }
+            }
+
+            // CREATE DYNAMIC TABLE.
+            var table = document.createElement("table");
+
+            // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+            var tr = table.insertRow(-1);                   // TABLE ROW.
+
+            for (var i = 0; i < col.length; i++) {
+                var th = document.createElement("th");      // TABLE HEADER.
+                th.innerHTML = col[i];
+                tr.appendChild(th);
+            }
+
+            // ADD JSON DATA TO THE TABLE AS ROWS.
+            for (var i = 0; i < grocerylist.length; i++) {
+
+                tr = table.insertRow(-1);
+
+                for (var j = 0; j < col.length; j++) {
+                    var tabCell = tr.insertCell(-1);
+                    tabCell.innerHTML = grocerylist[i][col[j]];
+                }
+            }
+
+            // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+            var divContainer = document.getElementById("showData");
+            divContainer.innerHTML = "";
+            divContainer.appendChild(table);
+
+
+
+        }
+    }
+    xhttp.open("GET", "./Grocery.json", true);
+    xhttp.send();
+    var butt = document.getElementById("butt");
+    butt.style.display="none";
+}
+
